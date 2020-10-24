e2e2490d18d9a7cfc5f4f16d6f75d3aebcb9173c
Grekus14 <georgepopovich14@gmail.com>
2020-10-24 20:22:23 +0200

    Implement user authentication

---
+++ routes/authRoutes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
+
+// Endpoint for creating an account
+router.post('/register', async (req, res) => {
+	const email = req.body.email;
+	const password = await bcrypt.hash(req.body.password, 10); // Use bcrypt to hash password
+
+	const newUser = new User({
+		email,
+		password
+	});
+
+	try {
+		await newUser.save();
+	} catch (err) {
+		// If email is already taken return a 403, otherwise return error.
+		if (err.code === 11000) return res.status(403).send('Email already taken.');
+		return res.send(err);
+	}
+
+	res.send(newUser);
+});
+
+// Endpoint for loggin in
+router.post('/login', async (req, res) => {
+	const email = req.body.email;
+	const password = req.body.password;
+
+	// Check if the user exists
+	const existingUser = await User.findOne({ email });
+	if (existingUser == null) return res.status(401).send('Incorrect email.');
+
+	// Check if the password is correct
+	if (await bcrypt.compare(password, existingUser.password)) {
+		res.send('Successfully logged in.');
+	} else res.send('Incorrect password.');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+app.use(express.json());
+
+// Connect to MongoDB Atlas
 mongoose.connect(process.env.MONGODB_ATLAS_URI, {
 	useNewUrlParser: true,
-	useUnifiedTopology: true
+	useUnifiedTopology: true,
+	useCreateIndex: true
 });
 const connection = mongoose.connection;
 connection.on('error', err => console.error(err));
 connection.on('open', () => console.log('Connected to MongoDB Atlas'));
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+// Connect app to all of the different routes
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
